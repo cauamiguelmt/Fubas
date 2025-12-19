d845ad29e62b4afe93affd7d3966513712abc769
cauamiguelmt.profissional@gmail.com <cauamiguelmt.profissional@gmail.com>
2025-12-19 17:57:44 -0300

    Generalização do comando user

--- src/commands/geral/user.ts
+++ src/commands/geral/user.ts
@@ -7,31 +7,56 @@ export default {
 
     async execute(interaction: CommandInteraction) {
         
+        // --- Simulação de perfil do usuário ---
+        const username = "Loopyng";
+        const userUrl = "https://osu.ppy.sh/users/loopyng";
+        const userAvatarUrl = "https://iili.io/fcihcyG.jpg";
+        const userRank = 1;
+        const userPP = "4,863.27";
+        const userAcc = 97.98;
+        const userLevel = "100 + 25.00";
+        const userPlaycount = "27,077";
+        const userPlaytime = 520;
+        const userRanks = {
+            XH: 2,
+            X: 5,
+            SH: 49,
+            S: 148,
+            A: 604
+        };
+        const userLastSeen: string = "3 days"; // "Tempo" ou "Online"
+        // ---------------------
+        
         // Emojis de rank
         const emojiXH = "<:rankingXHsmall2x:1451026695569281146>";
         const emojiX = "<:rankingXsmall2x:1451026724513906698>"; 
         const emojiSH = "<:rankingSHsmall2x:1451026620310753382>";
         const emojiS = "<:rankingSsmall2x:1451026644579127476>"; 
         const emojiA = "<:rankingAsmall2x:1451026496968986795>";
-
+        
+        const displayLastSeen = userLastSeen === "Online" ? "Online on osu! Fubika" : `Last Seen ${userLastSeen} Ago on osu`;
+        const displayLastSeenIcon = userLastSeen === "Online" ? // Green dot : Red dot
+                                                                'https://www.freepnglogos.com/uploads/dot-png/green-dot-clip-art-clkerm-vector-clip-art-online-10.png'
+                                                              : 'https://www.freepnglogos.com/uploads/dot-png/red-glossy-dot-clip-art-clkerm-vector-clip-art-18.png';
+        
         const embed = new EmbedBuilder()
         .setAuthor({ 
-            name: 'osu! Standard Profile for Loopyng', 
+            name: `osu! Standard Profile for ${username}`, 
             iconURL: 'https://iili.io/fcDwBEJ.png', // URL do Fubas.png
-            url: 'https://osu.ppy.sh/users/loopyng' // Link para o perfil do usuário
+            url: userUrl
         })
-        .setColor("#436990") // Azul Escuro
-        .setThumbnail('https://iili.io/fcihcyG.jpg') // URL do avatar do usuário
+        .setColor("#4189D3") // Azul
+        .setThumbnail(userAvatarUrl)
         .setDescription(`
-• **Fubika Rank:** \`#1\`
-• **PP:** \`4,863.27\` • **Acc:** \`97.98%\`
-• **Level:** \`100 + 25.00%\`
-• **Playcount:** \`27,077\` (\`520 hrs\`)
-• **Ranks:** ${emojiXH} \`2\` ${emojiX} \`5\` ${emojiSH} \`49\` ${emojiS} \`148\` ${emojiA} \`604\`
+• **Fubika Rank:** \`#${userRank}\`
+• **PP:** \`${userPP}\` • **Acc:** \`${userAcc}%\`
+• **Level:** \`${userLevel}%\`
+• **Playcount:** \`${userPlaycount}\` (\`${userPlaytime} hrs\`)
+• **Ranks:** ${emojiXH} \`${userRanks.XH}\` ${emojiX} \`${userRanks.X}\` ${emojiSH} \`${userRanks.SH}\` ${emojiS} \`${userRanks.S}\` ${emojiA} \`${userRanks.A}\`
         `)
         .setFooter({ 
-            text: 'Last Seen 3 Days Ago on osu! Fubika', 
-            iconURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Basic_red_dot.png/600px-Basic_red_dot.png'
+            text: displayLastSeen, 
+            iconURL: displayLastSeenIcon
         });
         
         interaction.reply({
